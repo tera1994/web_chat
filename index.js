30a3e4d3f29bbad66359aca50da85f1e62a91cd2
kyono hiroki <hiro_1567souls@icloud.com>
2020-08-05 04:26:43 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port=8080;
+
+app.use(express.static('./'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'\\main.html');
+});
+
+app.listen(port, ()=> {
+    console.log('Server started on port:'+port);
+});
