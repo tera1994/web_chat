79862c4f1db4e363b11a8ad9147aa11d063fc258
kyono hiroki <hiro_1567souls@icloud.com>
2020-08-10 14:06:39 +0900

    websocketのサーバコード追加

--- app.js
+++ app.js
@@ -2,11 +2,41 @@
 
 var express = require("express");
 var app = express();
+var https = require("https");
+var fs = require("fs");
+var ws = require("ws");
+
+var server = {port: 8080}
+if(process.argv[2] == "product"){
+  server = https.createServer({
+    cert: fs.readFileSync(__dirname+'/greenlock.d/live/chattest.ddns.net/cert.pem'),
+    key: fs.readFileSync(__dirname+'/greenlock.d/live/chattest.ddns.net/privkey.pem')
+  });
+  console.log("honban mode")
+}
+const wss = new ws.Server({ server });
+wss.on('connection', function connection(ws,req) {
+    const ip = req.connection.remoteAddress;
+    console.log('---connect from ' + ip);
+    ws.send('Hello from wssServer!');
+    ws.on('message', function incoming(message) {
+        console.log(message);
+    });
+	ws.onerror = (event) => {
+		console.error(event.message);
+		console.log(event)
+		ws.close();
+	};
+});
+if(process.argv[2] == "product"){
+  server.listen(8080)
+}
 
 app.use(express.static('./public/'));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname+'/public/html/main.html');
+    res.sendFile(__dirname+'/public/html/test.html');
+    console.log(req.ip)
 });
 
 
