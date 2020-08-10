619aafad17f20ea1697bf09d3d902e18266ffb76
Terazumi <>
2020-08-10 14:00:50 +0900

    modify chat_client.js

--- public/js/chat_client.js
+++ public/js/chat_client.js
@@ -7,12 +7,14 @@ ws.addEventListener('open', e => {
 
 // サーバからのデータ受信時に呼ばれる
 ws.addEventListener('message', e => {
-    document.getElementById("span1").textContent += e.data;
+    //document.getElementById("span1").textContent += e.data;
+    document.getElementById('wrap').innerHTML += (e.data+"</br>");
 })
 
 function send_text() {
     let input_message = document.getElementById("input_message").value;
     ws.send(input_message);
-    document.getElementById("span1").textContent += input_message + '\n';
+
+    document.getElementById('wrap').innerHTML += (input_message+"</br>");
     document.getElementById("input_message").value = "";
 }
