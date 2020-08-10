0c17f8d04679e4fd1094ed39b7861e1d8a024fb0
kyono hiroki <hiro_1567souls@icloud.com>
2020-08-10 14:36:13 +0900

    バグ修正

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ var express = require("express");
 var app = express();
 var https = require("https");
 var fs = require("fs");
-var ws = require("ws");
+var WebSocket = require("ws");
 
-var server = {port: 8080}
+var server;
+var wss;
 if(process.argv[2] == "product"){
   server = https.createServer({
     cert: fs.readFileSync(__dirname+'/greenlock.d/live/chattest.ddns.net/cert.pem'),
     key: fs.readFileSync(__dirname+'/greenlock.d/live/chattest.ddns.net/privkey.pem')
   });
   console.log("honban mode")
+  wss = new WebSocket.Server({ server });
+}else{
+  wss = new WebSocket.Server({ port: 8080 });
 }
-const wss = new ws.Server({ server });
 wss.on('connection', function connection(ws,req) {
     const ip = req.connection.remoteAddress;
     console.log('---connect from ' + ip);
+    // console.log(req.connection)
     ws.send('Hello from wssServer!');
     ws.on('message', function incoming(message) {
         console.log(message);
